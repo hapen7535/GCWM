db53f5a1d3f62ad40d18a465b2f37b85d9d1e0c1
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-02-07 19:47:22 +0900

    feat : 레시피 찾기 버튼 textView에서 Button으로 변경

---
+++ gcwm/app/src/main/java/com/example/gcwm/RecipesList.kt
@@ -0,0 +1,11 @@
+package com.example.gcwm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipes_list)
+    }
+}
