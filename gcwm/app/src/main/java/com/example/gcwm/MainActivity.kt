8a7926f951302b9ba9a3dbc5eeec858df357459d
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-02-13 19:24:34 +0900

    feat : 메인 화면 터치 시 재료 추가 화면으로 이동

--- gcwm/app/src/main/java/com/example/gcwm/MainActivity.kt
+++ gcwm/app/src/main/java/com/example/gcwm/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.gcwm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val intent = Intent(this, AddIngr::class.java)
+
+        when(event?.actionMasked){
+            MotionEvent.ACTION_DOWN -> {
+                startActivity(intent)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                startActivity(intent)
+            }
+            MotionEvent.ACTION_UP -> {
+                startActivity(intent)
+            }
+        }
+
+        return super.onTouchEvent(event)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        }
+
     }
-}
