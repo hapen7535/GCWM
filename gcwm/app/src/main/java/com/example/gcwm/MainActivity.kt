13aaa1744bb7697c9cb3694e7b789a5d4aa4bcd1
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-02-15 23:14:21 +0900

    feat : 메인 화면 MainText 부분 애니메이션 적용

--- gcwm/app/src/main/java/com/example/gcwm/MainActivity.kt
+++ gcwm/app/src/main/java/com/example/gcwm/MainActivity.kt
@@ -1,28 +1,50 @@
 package com.example.gcwm
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintSet
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var MainText : TextView
+
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+
         val intent = Intent(this, AddIngr::class.java)
+        MainText = findViewById<TextView>(R.id.mainText)
+        val anim: Animation = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = 50
+        anim.startOffset = 20
+        anim.repeatMode = Animation.REVERSE
+        anim.repeatCount = Animation.INFINITE
 
         when(event?.actionMasked){
             MotionEvent.ACTION_DOWN -> {
+                MainText.startAnimation(anim)
                 startActivity(intent)
             }
             MotionEvent.ACTION_MOVE -> {
+                MainText.startAnimation(anim)
                 startActivity(intent)
             }
             MotionEvent.ACTION_UP -> {
+                MainText.startAnimation(anim)
                 startActivity(intent)
             }
         }
+
+
+
 
         return super.onTouchEvent(event)
     }
@@ -35,3 +57,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
