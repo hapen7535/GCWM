a9a44787963ee52648754a519879f24d00f098f3
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-02-22 21:47:52 +0900

    feat : 레시피 결과 화면 내 뒤로 가기 버튼 활성화

--- gcwm/app/src/main/java/com/example/gcwm/RecipesList.kt
+++ gcwm/app/src/main/java/com/example/gcwm/RecipesList.kt
@@ -1,11 +1,28 @@
 package com.example.gcwm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RecipesList : AppCompatActivity() {
+
+    lateinit var returnBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipes_list)
+
+        returnBtn = findViewById<Button>(R.id.returnBtn)
+
+        returnBtn.setOnClickListener{
+
+            var intent = Intent(this, AddIngr::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
