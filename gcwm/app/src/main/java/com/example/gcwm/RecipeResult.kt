56454dcf7404d6942a0106029b1173771142de15
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-02-23 23:20:44 +0900

    feat : 상세 레시피 화면 내 홈버튼 활성화

--- gcwm/app/src/main/java/com/example/gcwm/RecipeResult.kt
+++ gcwm/app/src/main/java/com/example/gcwm/RecipeResult.kt
@@ -1,11 +1,26 @@
 package com.example.gcwm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RecipeResult : AppCompatActivity() {
+
+    lateinit var homeBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_result)
+
+        homeBtn = findViewById<Button>(R.id.backHome)
+
+        homeBtn.setOnClickListener{
+
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
